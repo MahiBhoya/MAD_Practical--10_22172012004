f890bce85de1b2be08328cc0ec739968215dc5c7
student <student@gnu.ac.in>
2023-10-27 12:14:33 +0530

    git

--- app/src/main/java/com/example/mad_practical_10_22172012004/PersonAdepter.kt
+++ app/src/main/java/com/example/mad_practical_10_22172012004/PersonAdepter.kt
@@ -3,9 +3,18 @@ package com.example.mad_practical_10_22172012004
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 
-class PersonAdapter(context:Context,val array: ArrayList<PersonClass>):ArrayAdapter<PersonClass>(context,0,array)
-{
-    override fun getView(position: Int, convertView: View?,
+class PersonAdapter(context:Context,val array:ArrayList<Person>):ArrayAdapter<Person>(context,0,array) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.parson_item,parent,false)
+        val person_data = array.get(position)
+        view.findViewById<TextView>(R.id.textView_name).text=person_data.name
+        view.findViewById<TextView>(R.id.textView_no).text=person_data.phoneNo
+        view.findViewById<TextView>(R.id.textView_email).text=person_data.emailId
+        view.findViewById<TextView>(R.id.textView_address).text=person_data.address
+        return view
+    }
 }
